34f0ae25a1a8049a67330226028fd64c9912a326
trinionggg <129654982+trinionggg@users.noreply.github.com>
2023-04-09 19:59:02 +0800

    Add files via upload

---
+++ IRequestView.java
@@ -0,0 +1,8 @@
+package interfaces;
+
+import Model.Request;
+
+public interface IRequestView {
+	public void displayRequestInfo(Request request);
+	
+}
---
+++ RequestView.java
@@ -0,0 +1,22 @@
+package view;
+import Model.Request;
+import interfaces.IRequestView;
+
+public class RequestView implements IRequestView{
+
+	public void displayRequestInfo(Request request) {
+		if (request.approve()) {
+			System.out.println("Your request has been approved.");
+		}
+		
+		else if (request.reject()) {
+			System.out.println("Your request has been rejected.");
+		}
+		
+		else {
+			System.out.println("Your request is pending.");
+		}
+		
+	}
+	
+}
