24e701aa088829a0affed743fd9cae4940e9fbfa
Toh Jing Hua <tohjinghua123@gmail.com>
2023-04-10 10:37:09 +0800

    view/approve/reject pending requests

--- src/controllers/SupervisorController.java
+++ src/controllers/SupervisorController.java
@@ -7,15 +7,23 @@ import java.util.Scanner;
 import enums.ProjectStatus;
 import interfaces.IProjectSupervisorService;
 import interfaces.IProjectView;
+import interfaces.IRequestSupervisorService;
+import interfaces.IRequestView;
 import models.Project;
+import models.Request;
 import services.ProjectSupervisorService;
+import services.RequestSupervisorService;
 import store.AuthStore;
+import views.RequestChangeProjectTitleView;
 import views.SubmittedProjectView;
 
 public class SupervisorController extends UserController {
     private static final Scanner sc = new Scanner(System.in);
 
-    private static final IProjectSupervisorService projectSupervisorService = new ProjectSupervisorService();
+    protected static final IProjectSupervisorService projectSupervisorService = new ProjectSupervisorService();
+    protected static final IRequestSupervisorService requestSupervisorService = new RequestSupervisorService();
+    protected static IProjectView projectView;
+    protected static IRequestView requestView;
 
     protected void createProjects() {
         System.out.print("Enter the number of projects to create: ");
@@ -71,7 +79,7 @@ public class SupervisorController extends UserController {
     }
 
     protected void viewProjects() {
-        IProjectView projectView = new SubmittedProjectView();
+        projectView = new SubmittedProjectView();
         String supervisorID = AuthStore.getCurrentUser().getUserID();
         ArrayList<Project> projects = projectSupervisorService.getSubmittedProjects(supervisorID);
 
@@ -83,7 +91,52 @@ public class SupervisorController extends UserController {
     }
 
     protected void viewApproveRejectPendingRequest() {
-        
+        String supervisorID = AuthStore.getCurrentUser().getUserID();
+        requestView = new RequestChangeProjectTitleView();
+        ArrayList<Request> requests = requestSupervisorService.getStudentPendingRequests(supervisorID);
+
+        while (true) {
+            System.out.println("requestID\tTitle");
+            requests.forEach(request -> requestView.displayRequestInfo(request));
+
+            System.out.println("Select requestID to approve/reject (Enter non-int to exit)");
+            if (!sc.hasNextInt()) {
+                sc.nextLine();
+                return;
+            }
+            int requestID = sc.nextInt();
+            sc.nextLine();
+
+            Optional<Request> optionalSelectedRequest = requests.stream()
+                    .filter(request -> request.getRequestID() == requestID)
+                    .findFirst();
+
+            if (optionalSelectedRequest.isPresent()) {
+                Request selectedRequest = optionalSelectedRequest.get();
+                System.out.println("\nSelect option (Enter non-int to exit):");
+                System.out.println("1. Approve");
+                System.out.println("2. Reject");
+
+                if (!sc.hasNextInt()) {
+                    sc.nextLine();
+                    return;
+                }
+                int option = sc.nextInt();
+                sc.nextLine();
+
+                switch (option) {
+                    case 1:
+                        selectedRequest.approve();
+                        break;
+                    case 2:
+                        selectedRequest.reject();
+                        break;
+                }
+                return;
+            } else {
+                System.out.println("Invalid requestID!");
+            }
+        }
     }
 
     public void start() {
