78249427ee24a7a0f27b3adb2959b37567c338c5
Jing Qiang <tohjingqiang@gmail.com>
2023-04-11 23:03:25 +0800

    feat: change passowrd requires new password to be different from old password

--- src/controllers/UserController.java
+++ src/controllers/UserController.java
@@ -10,7 +10,7 @@ public class UserController {
 
     protected boolean changePassword() {
         String oldPassword, newPassword;
-        boolean success;
+        boolean success = false;
         IUserService userService = new UserService();
 
         System.out.println("Changing Password...");
@@ -25,6 +25,14 @@ public class UserController {
 
             System.out.print("Enter new password: ");
             newPassword = sc.next();
+            
+            // New password must be different from old password
+            if (newPassword.equals(oldPassword)) {
+            	System.out.println("New password cannot match old password!");
+            	continue;
+            }
+            
+            // Change password
             success = userService.changePassword(oldPassword, newPassword);
 
             if (!success) {
