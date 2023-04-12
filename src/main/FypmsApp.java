b11ed1d4ccdb968010a5abbb81c74430f0b770d7
Jing Qiang <tohjingqiang@gmail.com>
2023-04-12 17:23:06 +0800

    exception handling for main app

--- src/main/FypmsApp.java
+++ src/main/FypmsApp.java
@@ -13,20 +13,21 @@ import views.CommonView;
 
 public class FypmsApp {
 	public static void main(String[] args) {
-		do {
-			// Initialize DataStore
-			DataStore.initDataStore(new CsvDataService(), FilePathsUtils.csvFilePaths());
-			
-			// Display Splash Screen
-			CommonView.printSplashScreen();
-			
-			// Authentication - Log In
-			AuthController.startSession();
-			if(!AuthStore.isLoggedIn()) return;
-			
-			// Start session
-			User user = AuthStore.getCurrentUser();
-			switch (user.getRole()) {
+		try {
+			do {
+				// Initialize DataStore
+				DataStore.initDataStore(new CsvDataService(), FilePathsUtils.csvFilePaths());
+				
+				// Display Splash Screen
+				CommonView.printSplashScreen();
+				
+				// Authentication - Log In
+				AuthController.startSession();
+				if(!AuthStore.isLoggedIn()) return;
+				
+				// Start session
+				User user = AuthStore.getCurrentUser();
+				switch (user.getRole()) {
 				case STUDENT:
 					new StudentController().start();
 					break;
@@ -36,7 +37,16 @@ public class FypmsApp {
 				case FYPCOORDINATOR:
 					new FYPCoordinatorController().start();
 					break;
-			}
-		} while (true);
+				}
+			} while (true);
+		} catch (Exception e) {
+			// Save Data and Logout user
+			DataStore.saveData();
+			AuthController.endSession();
+			
+			// Print message
+			System.out.println("FYPMS crashed. Please restart the system.");
+			System.out.println("Error: " + e.getMessage());
+		}
 	}
 }
