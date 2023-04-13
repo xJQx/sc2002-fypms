07a5b8a032dbd1f7237dab3c1efdef84caa07ad9
Jing Hua <tohjinghua123@gmail.com>
2023-04-13 14:17:09 +0800

    javadocs generation instructions

--- src/module-info.java
+++ src/module-info.java
@@ -3,7 +3,18 @@
  */
 /**
  * @author Toh Jing Qiang
+ * @author Toh Jing Hua
+ * @author Trini
  *
  */
 module sc2002_fypms {
+    exports controllers;
+    exports enums;
+    exports interfaces;
+    exports main;
+    exports models;
+    exports services;
+    exports stores;
+    exports utils;
+    exports views;
 }
