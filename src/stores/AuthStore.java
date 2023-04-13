308a2a69e92bc9b03b9c5fe43dfbd3f4a10571a6
Jing Hua <tohjinghua123@gmail.com>
2023-04-13 12:47:22 +0800

    javadocs: store

--- src/stores/AuthStore.java
+++ src/stores/AuthStore.java
@@ -2,21 +2,41 @@ package stores;
 
 import models.User;
 
+/**
+ * The {@link AuthStore} class provides utility methods for managing the current
+ * authenticated user within the application. It offers methods to set and get
+ * the current user, as well as check if the user is logged in.
+ */
 public class AuthStore {
 	private static User currentUser;
-	
-	private AuthStore() {}
-	
+
+	private AuthStore() {
+	}
+
+	/**
+	 * Sets the current user in the AuthStore.
+	 *
+	 * @param currentUser the {@link User} instance to set as the current user
+	 */
 	public static void setCurrentUser(User currentUser) {
 		AuthStore.currentUser = currentUser;
 	}
-	
+
+	/**
+	 * Checks if the user is logged in.
+	 *
+	 * @return {@code true} if the user is logged in, {@code false} otherwise
+	 */
 	public static boolean isLoggedIn() {
 		return currentUser != null;
 	}
-	
+
 	// ---------- Get Methods ---------- //
-	
+	/**
+	 * Gets the current user from the AuthStore.
+	 *
+	 * @return the current {@link User} instance, or {@code null} if not logged in
+	 */
 	public static User getCurrentUser() {
 		return AuthStore.currentUser;
 	}
--- src/stores/DataStore.java
+++ src/stores/DataStore.java
@@ -10,6 +10,12 @@ import models.Request;
 import models.Student;
 import models.Supervisor;
 
+/**
+ * The {@link DataStore} class provides utility methods for managing data
+ * storage within the application. It offers methods to initialize the data
+ * store, import and export data to and from the file system, and interact with
+ * data maps for various data types.
+ */
 public class DataStore {
 	private static IFileDataService fileDataService;
 	private static Map<String, String> filePathsMap;
@@ -23,6 +29,17 @@ public class DataStore {
 	private DataStore() {
 	}
 
+	/**
+	 * Initializes the DataStore by setting up the file data service, file paths
+	 * map, and importing data from the file system.
+	 *
+	 * @param fileDataService the {@link IFileDataService} instance to use for data
+	 *                        operations
+	 * @param filePathsMap    the {@link Map} containing file paths for various data
+	 *                        types
+	 * @return {@code true} if the initialization is successful, {@code false}
+	 *         otherwise
+	 */
 	public static boolean initDataStore(IFileDataService fileDataService, Map<String, String> filePathsMap) {
 		// Initialize fileDataService and filePathsMap
 		DataStore.filePathsMap = filePathsMap;
@@ -44,6 +61,12 @@ public class DataStore {
 		return true;
 	}
 
+	/**
+	 * Saves the data from the DataStore to the file system.
+	 *
+	 * @return {@code true} if the data is saved successfully, {@code false}
+	 *         otherwise
+	 */
 	public static boolean saveData() {
 		DataStore.setStudentsData(studentsData);
 		DataStore.setSupervisorsData(supervisorsData);
@@ -55,30 +78,67 @@ public class DataStore {
 	}
 
 	// ---------- Student ---------- //
+	/**
+	 * Gets the students data map.
+	 *
+	 * @return a {@link Map} containing student ID as the key and {@link Student}
+	 *         objects as the value
+	 */
 	public static Map<String, Student> getStudentsData() {
 		return DataStore.studentsData;
 	}
 
+	/**
+	 * Sets the students data map and saves the data to the file system.
+	 *
+	 * @param studentsData a {@link Map} containing student ID as the key and
+	 *                     {@link Student} objects as the value
+	 */
 	public static void setStudentsData(Map<String, Student> studentsData) {
 		DataStore.studentsData = studentsData;
 		fileDataService.exportStudentData(filePathsMap.get("user"), filePathsMap.get("student"), studentsData);
 	}
 
 	// ---------- Supervisor ---------- //
+	/**
+	 * Gets the supervisors data map.
+	 *
+	 * @return a {@link Map} containing supervisor ID as the key and
+	 *         {@link Supervisor} objects as the value
+	 */
 	public static Map<String, Supervisor> getSupervisorsData() {
 		return DataStore.supervisorsData;
 	}
 
+	/**
+	 * Sets the supervisors data map and saves the data to the file system.
+	 *
+	 * @param supervisorsData a {@link Map} containing supervisor ID as the key and
+	 *                        {@link Supervisor} objects as the value
+	 */
 	public static void setSupervisorsData(Map<String, Supervisor> supervisorsData) {
 		DataStore.supervisorsData = supervisorsData;
 		fileDataService.exportSupervisorData(filePathsMap.get("user"), filePathsMap.get("supervisor"), supervisorsData);
 	}
 
 	// ---------- FYP Coordinator ---------- //
+	/**
+	 * Gets the FYP coordinators data map.
+	 *
+	 * @return a {@link Map} containing FYP coordinator ID as the key and
+	 *         {@link FYPCoordinator} objects as the value
+	 */
 	public static Map<String, FYPCoordinator> getFYPCoordinatorsData() {
 		return DataStore.fypcoordinatorsData;
 	}
 
+	/**
+	 * Sets the FYP coordinators data map and saves the data to the file system.
+	 *
+	 * @param fypcoordinatorsData a {@link Map} containing FYP coordinator ID as the
+	 *                            key and {@link FYPCoordinator} objects as the
+	 *                            value
+	 */
 	public static void setFYPCoordinatorsData(Map<String, FYPCoordinator> fypcoordinatorsData) {
 		DataStore.fypcoordinatorsData = fypcoordinatorsData;
 		fileDataService.exportFYPCoordinatorData(filePathsMap.get("user"), filePathsMap.get("supervisor"),
@@ -86,20 +146,44 @@ public class DataStore {
 	}
 
 	// ---------- Project ---------- //
+	/**
+	 * Gets the projects data map.
+	 *
+	 * @return a {@link Map} containing project ID as the key and {@link Project}
+	 *         objects as the value
+	 */
 	public static Map<Integer, Project> getProjectsData() {
 		return DataStore.projectsData;
 	}
 
+	/**
+	 * Sets the projects data map and saves the data to the file system.
+	 *
+	 * @param projectsData a {@link Map} containing project ID as the key and
+	 *                     {@link Project} objects as the value
+	 */
 	public static void setProjectsData(Map<Integer, Project> projectsData) {
 		DataStore.projectsData = projectsData;
 		fileDataService.exportProjectData(filePathsMap.get("project"), projectsData);
 	}
 
 	// ---------- Request ---------- //
+	/**
+	 * Gets the requests data map.
+	 *
+	 * @return a {@link Map} containing requestID as the key and {@link Request}
+	 *         objects as the value
+	 */
 	public static Map<Integer, Request> getRequestsData() {
 		return DataStore.requestData;
 	}
 
+	/**
+	 * Sets the requests data map and saves the data to the file system.
+	 *
+	 * @param requestData a {@link Map} containing request ID as the key and
+	 *                    {@link Request} objects as the value
+	 */
 	public static void setRequestsData(Map<Integer, Request> requestData) {
 		DataStore.requestData = requestData;
 		fileDataService.exportRequestData(filePathsMap.get("request"), filePathsMap.get("transferStudentRequest"),
