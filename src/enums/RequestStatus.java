5c3a213d3688031d1ba0c1ad955b7671dc6908b2
Jing Hua <tohjinghua123@gmail.com>
2023-04-13 12:16:23 +0800

    javadocs: enum

--- src/enums/RequestStatus.java
+++ src/enums/RequestStatus.java
@@ -1,5 +1,14 @@
 package enums;
 
+/**
+ * The {@link RequestStatus} enumeration represents the different states a
+ * request can be in.
+ * <ul>
+ * <li>{@link #PENDING}: The request is awaiting approval or rejection.</li>
+ * <li>{@link #APPROVED}: The request has been approved.</li>
+ * <li>{@link #REJECTED}: The request has been rejected.</li>
+ * </ul>
+ */
 public enum RequestStatus {
 	PENDING,
 	APPROVED,
