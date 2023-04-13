ec4a2d77658b09227ed2bc9dca72e2abc45c90c2
Jing Hua <tohjinghua123@gmail.com>
2023-04-13 13:08:40 +0800

    javadocs: services

--- src/services/AuthService.java
+++ src/services/AuthService.java
@@ -4,15 +4,27 @@ import interfaces.IAuthService;
 import models.User;
 import stores.AuthStore;
 
+/**
+ * The {@link AuthService} class is an abstract class that implements the
+ * {@link IAuthService} interface.
+ * It provides basic authentication functionalities for user login and logout.
+ */
 public abstract class AuthService implements IAuthService {
-	public abstract boolean login(String userID, String password);
-	
+    public abstract boolean login(String userID, String password);
+
     @Override
     public boolean logout() {
         AuthStore.setCurrentUser(null);
         return true;
     };
 
+    /**
+     * Authenticates the given user with the given password.
+     * 
+     * @param user     the user to be authenticated
+     * @param password the password to be used for authentication
+     * @return true if the user is authenticated successfully, false otherwise
+     */
     protected boolean authenticate(User user, String password) {
         if (user == null)
             return false;
